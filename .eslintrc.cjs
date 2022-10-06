18ea8c8aa8ce966ea8da5c03e7adbadd7ead289f
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-10-06 22:29:52 +0900

    ESLintとPrettier、lint-staged を追加

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-key': [
+      'error',
+      { checkFragmentShorthand: true, warnOnDuplicates: true },
+    ],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
