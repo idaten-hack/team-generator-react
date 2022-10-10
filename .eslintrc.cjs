c26d28100d1cd23b47d690a65e7171406ec5a180
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-10-10 11:49:21 +0900

    eslint-import-order の追加

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,16 +10,39 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', 'react-hooks'],
+  plugins: ['react', 'react-hooks', 'import'],
   rules: {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/jsx-key': [
       'error',
-      { checkFragmentShorthand: true, warnOnDuplicates: true },
+      {checkFragmentShorthand: true, warnOnDuplicates: true},
     ],
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index', 'object', 'type'],
+        pathGroups: [
+          {
+            pattern: '{react,react-dom/**,react-router-dom}',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: '@src/**',
+            group: 'parent',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: {
+          order: 'asc',
+        },
+        'newlines-between': 'always',
+      },
+    ],
   },
   settings: {
     react: {
