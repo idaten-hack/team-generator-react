f198f7c085c7ad73e92bde3186c509d501d8b9c1
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-10-10 21:54:35 +0900

    変更

--- src/components/SampleComponent.jsx
+++ src/components/SampleComponent.jsx
@@ -20,9 +20,6 @@ export default function SampleComponent() {
       <Button variant="contained" color="info">
         info
       </Button>
-      <Button variant="contained" color="success">
-        success
-      </Button>
     </Stack>
   )
 }
