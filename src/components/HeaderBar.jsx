70dbe7555cbf89a3100a0b84cc255e75a6e389f1
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-10-10 21:24:32 +0900

    中央揃え

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -1,6 +1,5 @@
 import Diversity3Icon from '@mui/icons-material/Diversity3'
 import AppBar from '@mui/material/AppBar'
-import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
@@ -8,7 +7,13 @@ import Typography from '@mui/material/Typography'
 export default function HeaderBar() {
   return (
     <AppBar position="static">
-      <Container maxWidth="xl">
+      <Container
+        maxWidth="xl"
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
         <Toolbar disableGutters>
           <Diversity3Icon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
           <Typography
@@ -17,7 +22,6 @@ export default function HeaderBar() {
             component="a"
             href="/"
             sx={{
-              mr: 2,
               display: { xs: 'none', md: 'flex' },
               fontWeight: 500,
               color: 'inherit',
@@ -27,7 +31,6 @@ export default function HeaderBar() {
             振り分けくん
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}></Box>
           <Diversity3Icon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
           <Typography
             variant="h6"
@@ -35,9 +38,7 @@ export default function HeaderBar() {
             component="a"
             href=""
             sx={{
-              mr: 2,
               display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
               fontWeight: 500,
               color: 'inherit',
               textDecoration: 'none',
@@ -45,9 +46,6 @@ export default function HeaderBar() {
           >
             振り分けくん
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}></Box>
-
-          <Box sx={{ flexGrow: 0 }}></Box>
         </Toolbar>
       </Container>
     </AppBar>
